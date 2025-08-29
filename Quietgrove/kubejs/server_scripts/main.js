96a311aceaed4ae017a5029f98cb3548e8e12cc1
CatchingVeil <catchingveil1@gmail.com>
2025-08-29 02:22:56 -0400

    Delete Quietgrove directory

--- Quietgrove/kubejs/server_scripts/main.js
+++
@@ -1,6 +0,0 @@
-// Visit the wiki for more info - https://kubejs.com/
-console.info('Hello, World! (Loaded server example script)')
-// Listen to item tag event
-ServerEvents.tags('item', event => {
-  // Get the #forge:cobblestone tag collection and add Diamond Ore to it
-  event.add('#c:strings', 'ists_harder_early:plant_string')})
